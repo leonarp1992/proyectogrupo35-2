74876709f176b211aa27f20a8f99b04f0a98f143
leonarp1992 <leonarp@uninorte.edu.co>
2021-10-19 21:02:30 -0500

    Login usuarios

--- app.py
+++ app.py
@@ -4,20 +4,94 @@ import yagmail
 import os
 import sqlite3
 from sqlite3 import Error
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-@app.route("/")
-def inicio():
-    return render_template("Login.html")
+# @app.route("/")
+# def inicio():
+#     return render_template("Login.html")
 
 @app.route("/", methods=["GET", "POST"])
 def login():
+    if request.method == "GET":
+        return render_template("Login.html")
+    
+    if request.method == "POST":
+        correo = request.form["correo"]
+        password = request.form["password"]
+        tipoPerfil = "Administrador"
+        try:
+            with sqlite3.connect('Plavue.db') as con: 
+             cur = con.cursor()
+             cur.execute("SELECT * FROM Usuarios WHERE correo=? AND contraseña =? AND tipoPerfil=?",[correo,password, tipoPerfil])
+             if cur.fetchone():
+                 return render_template("perfiladmin.html")
+             elif request.method == "POST":
+                 correo = request.form["correo"]
+                 password = request.form["password"]
+                 tipoPerfil = "Piloto"
+                 try:
+                    with sqlite3.connect('Plavue.db') as con: 
+                     cur = con.cursor()
+                     cur.execute("SELECT * FROM Usuarios WHERE correo=? AND contraseña =? AND tipoPerfil=?",[correo,password, tipoPerfil])
+                     if cur.fetchone():
+                         return render_template("perfilpiloto.html")
+                     elif request.method == "POST":
+                        correo = request.form["correo"]
+                        password = request.form["password"]
+                        try:
+                            with sqlite3.connect('Plavue.db') as con: 
+                             cur = con.cursor()
+                             cur.execute("SELECT * FROM Usuarios WHERE correo=? AND contraseña =?",[correo,password])
+                             if cur.fetchone():
+                                return render_template("perfilusuario.html")
+                             else:
+                                return "Usuario no permitido"
+                        except Error as er:
+                            print('SQLite error: %s' % (' '.join(er.args)))
+                            print('SQLite traceback: ')
+                 except Error as er:
+                    print('SQLite error: %s' % (' '.join(er.args)))
+                    print('SQLite traceback: ')
+        except Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print('SQLite traceback: ')
     return render_template("Login.html")
 
 @app.route("/registro", methods=["GET", "POST"])
 def registro():
+    now = datetime.now()
+    id = now.strftime("%Y%m%d%H%M%S")
+    print(id)
+    if request.method == 'POST':
+        now = datetime.now()
+        id = now.strftime("%Y%m%d%H%M%S")
+        nombre = request.form["Nombre"]
+        correo = request.form["correo"]
+        password = request.form["password"]        
+        try:
+            with sqlite3.connect('Plavue.db') as con: #establecer objeto conexion a base de datos
+                cur = con.cursor() #manipular la conexión a la bd
+                cur.execute('INSERT INTO Usuarios (idUsuario, contraseña, Nombre, correo) VALUES (?,?,?,?)', (id, password, nombre,correo))
+                con.commit() #confirmar la transacción
+                return render_template("Login.html")
+        except Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print('SQLite traceback: ')
+            return "No se pudo guardar"
+    return render_template("registro.html")
+   
+@app.route("/recuperarcontraseña", methods=["GET", "POST"])
+def recuperar_contraseña():
+    return render_template('recuperarcontraseña.html')
+
+@app.route("/perfiladmin", methods=["GET", "POST"])
+def perfil_admin():
+    # if request.method == "GET":
+
+     
     if request.method == 'POST':
         nombre = request.form["Nombre"]
         correo = request.form["correo"]
@@ -32,14 +106,6 @@ def registro():
             print('SQLite error: %s' % (' '.join(er.args)))
             print('SQLite traceback: ')
             return "No se pudo guardar"
-    return render_template("registro.html")
-   
-@app.route("/recuperarcontraseña", methods=["GET", "POST"])
-def recuperar_contraseña():
-    return render_template('recuperarcontraseña.html')
-
-@app.route("/perfiladmin", methods=["GET", "POST"])
-def perfil_admin():
     return render_template("perfiladmin.html")
 
 @app.route("/GestionPilotos", methods=["GET"])
